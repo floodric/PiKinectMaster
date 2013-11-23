8ceeef9f3c71308dfbed1ac18c203b8830b8e799
root <root@floodric.(none)>
2013-11-23 19:24:40 +0000

    master code forraspberry pi pretty well set up, just needs pi to connect properly now

--- server.js
+++ server.js
@@ -1,37 +1,55 @@
-var http = require("http");
+var express = require('express'),
+    cons = require ('consolidate'),
+//    routes = require('./routes/slash'),
+//    course = require('./routes/course'),
+//    update = require('./routes/update'),
+//    model = require('./model/model.js'),
+    http = require('http'),
+    path = require('path'),
+    net = require('net');
 
-var basicOptions = {
-  hostname: "rota.praetorian.com",
-  path: "/rota/service/play.php?place=4",
-  method: "GET",
-  port: "80"
-}
+var app = express();
 
-var interface = {
-  "new":"",
-  place:"location=%d", 
-  move:"from=%d&to=%d",
-  "status":""
-}
-
-function handleRequest(req,resp){
-  console.log(req.headers);
-  console.log("Request Received"+req.url);
-  resp.writeHead(200, {"Content-Type": "text/plain"});
-  resp.write(req.method+' is how you got here');
-  resp.end();
-}
-
-http.createServer(handleRequest).listen(8888);
-
-var req = http.request(basicOptions,function(resp){
-            console.log(resp);
-          });
-
-req.on('error', function(e) {
-  console.log('problem with request: ' + e.message);
+var server = net.createServer(function(c){
+  counter = 0;
+  console.log('server connected');
+  c.on('end',function(){
+    console.log('server disconnected');
+  });
+  c.on('data', function(data){
+    console.log(data);
+    c.write('let me show you right now fore you give it up');
+    if(counter++ == 5){
+      c.end('hell give us what we want');
+    }
+  });
 });
 
-req.write('data\n');
-req.write('data\n');
-req.end();
+server.listen(4141,"floodric.com", function(){
+  console.log('server bound 2 fall in love');
+  console.log(JSON.stringify(server.address()));
+});
+
+console.log('here?');
+
+app.get('/', function(req,res){
+  res.writeHead(200,{"Content-Type":"text/plain"});
+  res.end("You reached me! you are home");
+});
+
+app.get('/boxes', function(req,res){
+  sockets.connect('http://' + piaddr);
+  res.writeHead(200,{"Content-Type":"text/plain"});
+  if(boxes.pi){
+    res.end("The Pi is listening!");
+  } else {
+    res.end("No pi, maybe refresh at some point?");
+  }
+});
+app.get('/:place', function(req,res){
+  res.writeHead(200,{"Content-Type":"text/plain"});
+  res.end("You reached me! you are looking for "+req.params.place);
+});
+
+var PORT = 8888;
+app.listen(PORT, function(){console.log('app also listening');});
