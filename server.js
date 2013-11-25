23cad581a8183a184cdc9b44f82889dcd3ce753f
floodric <rflood@cmu.edu>
2013-11-25 16:47:45 +0000

    I was stupid, i need to change this form net server to client n thats it

--- server.js
+++ server.js
@@ -1,4 +1,12 @@
 var net = require('net');
+var express = require('express'),
+    cons = require ('consolidate'),
+// routes = require('./routes/slash'),
+// course = require('./routes/course'),
+// update = require('./routes/update'),
+// model = require('./model/model.js'),
+    http = require('http'),
+    path = require('path');
 
 var app = express();
 var piOnline = false;
@@ -30,3 +38,26 @@ server.listen(4141,"badhost", function(){
   console.log('listening');
   console.log(JSON.stringify(server.address()));
 });
+
+
+app.get('/', function(req,res){
+  res.writeHead(200,{"Content-Type":"text/plain"});
+  res.end("You reached me! you are home");
+});
+
+app.get('/boxes', function(req,res){
+  sockets.connect('http://' + piaddr);
+  res.writeHead(200,{"Content-Type":"text/plain"});
+  if(boxes.pi){
+    res.end("The Pi is listening!");
+  } else {
+    res.end("No pi, maybe refresh at some point?");
+  }
+});
+app.get('/:place', function(req,res){
+  res.writeHead(200,{"Content-Type":"text/plain"});
+  res.end("You reached me! you are looking for "+req.params.place);
+});
+
+var PORT = 8888;
+app.listen(PORT, function(){console.log('app also listening');});
